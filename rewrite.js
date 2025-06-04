a960ba6f80b42e56baaa77bda91e951eb5564171
ItMe12s <shinnapat06603@varee.ac.th>
2025-06-04 21:13:50 +0700

    rewrite file started

---
+++ rewrite.js
@@ -0,0 +1,11 @@
+// Made by @ludQC_QA on X.com (Twitter)
+
+const config = {
+    // Your username and password on YRC canteen
+    username: "50000",
+    password: "12345",
+
+    // Customizations
+    title: "YRC Canteen Widget",
+    sub_text: "┬┴┬┴┤(·_├┬┴┬┴",
+}
